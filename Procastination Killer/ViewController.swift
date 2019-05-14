b0c51e83b8f4c2907cc85890b69d6f4d4dfb31d2
Deepson Khadka <khaddeep@oisu.edu>
2019-05-13 21:47:16 -0400

    Initial Commit

---
+++ Procastination Killer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Procastination Killer
+//
+//  Created by Deepson on 5/13/19.
+//  Copyright © 2019 Deepson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
